8b8c7b05ea51dcf07520e954cd83e4cb0ea52555
zy_guang <nguyenduyquang6122.gmail.com>
2023-11-09 14:14:05 +0700

    update GUI

--- BaiTH_01.py
+++ BaiTH_01.py
@@ -2,30 +2,48 @@ from tkinter import *
 import numpy as np
 
 def update_n():
-    global n
-    n = int(entry_n.get())
+    try:
+        result_label.config(text="")
+        global n
+        n = int(entry_n.get())
+    except:
+        result_label.config(text="Hãy nhập 1 số dương\n")
 
-    label_A = Label(frame_A, text="Nhập ma trận hệ số A:")
-    label_A.pack()
+    if (n > 0):
+        result_label.config(text="")
 
-    global  entry_A
-    entry_A = []
-    for i in range(n):
-        row = Frame(frame_A)
-        row.pack()
-        entry_A.append([])
-        for j in range(n):
-            entry_A[i].append(Entry(row))
-            entry_A[i][j].pack(side=LEFT)
+        # Destroy old labels
+        for widget in frame_A.winfo_children():
+            widget.destroy()
 
-    label_B = Label(frame_B, text="Nhập vector kết quả B:")
-    label_B.pack()
+        for widget in frame_B.winfo_children():
+            widget.destroy()
 
-    global entry_B
-    entry_B = []
-    for i in range(n):
-        entry_B.append(Entry(frame_B))
-        entry_B[i].pack()
+        label_A = Label(frame_A, text="")
+        label_A.config(text="Nhập ma trận hệ số A:")
+        label_A.pack()
+
+        global  entry_A
+        entry_A = []
+        for i in range(n):
+            row = Frame(frame_A)
+            row.pack()
+            entry_A.append([])
+            for j in range(n):
+                entry_A[i].append(Entry(row))
+                entry_A[i][j].pack(side=LEFT)
+
+        label_B = Label(frame_B, text="")
+        label_B.config(text="Nhập vector kết quả B:")
+        label_B.pack()
+
+        global entry_B
+        entry_B = []
+        for i in range(n):
+            entry_B.append(Entry(frame_B))
+            entry_B[i].pack()
+    else:
+        result_label.config(text="Hãy nhập 1 số dương\n")
 
 def solve_equations():
     A = np.zeros((n, n))
@@ -33,16 +51,24 @@ def solve_equations():
 
     for i in range(n):
         for j in range(n):
-            A[i][j] = float(entry_A[i][j].get())
+            try:
+                A[i][j] = float(entry_A[i][j].get())
+            except ValueError:
+                result_label.config(text="Hãy nhập số hợp lệ cho"+f" A[{i+1}][{j+1}]"+"\n")
+                return
 
     for i in range(n):
-        B[i] = float(entry_B[i].get())
+        try:
+            B[i] = float(entry_B[i].get())
+        except ValueError:
+            result_label.config(text=f"Hãy nhập số hợp lệ cho B[{i+1}]\n")
+            return
 
     try:
         # Kiểm tra trường hợp ma trận A và vector kết quả B đều toàn số 0
         # toàn bộ các giá trị đã nhập
         if np.all(A == 0) and np.all(B == 0):
-            print("Hệ phương trình vô số nghiệm.")
+            result_label.config(text="Hệ phương trình vô nghiệm:\n")
         else:
             # Tính bậc thang của ma trận A
             rref_A, _ = np.linalg.qr(A)
@@ -57,7 +83,7 @@ def solve_equations():
 
             # Kiểm tra và in kết quả
             if num_free_variables > 0:
-                result_label.config(text="Nghiệm của hệ phương trình:\n")
+                result_label.config(text="Hệ phương trình vô số nghiệm:\n")
             elif num_free_variables == 0:
                 X = np.linalg.solve(A, B)
                 result = []
