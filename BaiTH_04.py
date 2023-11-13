8c7b3cd155aa75cf7bf6b8362823664653613fde
zy_guang <nguyenduyquang6122.gmail.com>
2023-11-13 23:38:19 +0700

    update

--- BaiTH_04.py
+++ BaiTH_04.py
@@ -1,37 +1,88 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
 from tkinter import *
 from tkinter import filedialog
-
-
 
 
 def open_file_dialog():
     file_path = filedialog.askopenfilename(initialdir="/", title="Chọn file", filetypes=(("CSV files", "*.csv"), ("all files", "*.*")))
     if file_path:
         display_csv_data(file_path)
-    global in_data
-    in_data = pd.read_csv(file_path, index_col = 0).values
-    print(in_data)
 
 def display_csv_data(file_path):
     # Clear existing widgets in the frame
     for widget in frame_data.winfo_children():
         widget.destroy()
 
-    # Read CSV data
-    csv_reader = pd.read_csv(file_path)
-    print(csv_reader)
-    header = next(csv_reader)  # Read the header
-    for col, header_value in enumerate(header):
-        label = tk.Label(frame, text=header_value, padx=10, pady=5)
+    # Read CSV data using pandas
+    df = pd.read_csv(file_path)
+    global in_data
+    in_data = pd.read_csv(file_path, index_col = 0).values
+
+    # Display header
+    for col, header_value in enumerate(df.columns):
+        label = Label(frame_data, text=header_value, padx=10, pady=5)
         label.grid(row=0, column=col)
 
-    for row, data in enumerate(csv_reader, start=1):
+    # Display data
+    for row, (_, data) in enumerate(df.iterrows(), start=1):
         for col, value in enumerate(data):
-            label = tk.Label(frame, text=value, padx=10, pady=5)
+            label = Label(frame_data, text=value, padx=10, pady=5)
             label.grid(row=row, column=col)
+
+def create_repost():
+    sv = in_data[:,1]
+    tongsv = sv.sum()
+
+    svF = in_data[:,10]
+    svDat = np.subtract(sv,svF)
+    svTruot = np.subtract(sv,svDat)
+    sumDat = svDat.sum()
+    sumTruot = svTruot.sum()
+    tyle_dat = np.divide(np.sum(svDat),np.sum(sv))*100
+    tyle_truot = 100 - tyle_dat
+
+    svA_B = in_data[:,2:5+1]
+    sumA_B = np.sum(svA_B)
+    svC_D = in_data[:,6:9+1]
+    sumC_D = np.sum(svC_D)
+    sumF = np.sum(svF)
+
+    svA = in_data[:,3]
+    maxA = svA.max()
+    i1 = np.where(svA == maxA)
+    maxF = svF.max()
+    minF = svF.min()
+    i2 = np.where(svF == minF)
+    i3 = np.where(svF == maxF)
+
+    label_result.config(text="-Tổng sinh viên dự thi: "+f"{tongsv} sinh viên\n"
+                        + "-Số sinh viên đạt: "+f"{sumDat} sinh viên\n"
+                        + "-Số sinh viên trượt: "+f"{sumTruot} sinh viên\n" 
+                        + "-Tỷ lệ sinh viên đạt: "+f"{tyle_dat} %\n"
+                        + "-Tỷ lệ sinh viên trượt: "+f"{tyle_truot} %\n"
+                        + "-Số sinh viên có điểm khá/giỏi là: "+f"{sumA_B} sinh viên\n"
+                        + "-Số sinh viên có điểm TB/yếu là: "+f"{sumC_D} sinh viên\n"
+                        + "-Số sinh viên có điểm kém (trượt) là: "+f"{sumF} sinh viên\n"
+                        + "-Lớp có nhiều điểm A nhất là: {0} có {1} sinh viên đạt điểm A\n".format(in_data[i1,0],maxA)
+                        + "-Lớp có ít điểm F nhất là: {0} có {1} sinh viên đạt điểm F\n".format(in_data[i2,0],minF)
+                        + "-Lớp có nhiều điểm F nhất là: {0} có {1} sinh viên đạt điểm F\n".format(in_data[i3,0],maxF))
+    
+    plot_frame = Frame(w)
+    plot_frame.pack()
+
+    plt.plot(range(len(in_data[:, 3])), in_data[:, 3], 'r-', label="Diem A")
+    plt.plot(range(len(in_data[:, 2:5+1])), in_data[:, 2:5+1], 'g-', label="Diem B +")
+    plt.xlabel('Lớp')
+    plt.ylabel('Số sv đạt điểm')
+    plt.legend(loc='upper right')
+
+    canvas = FigureCanvasTkAgg(plt.gcf(), master=plot_frame)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    
 
 w = Tk()
 w.title("Báo cáo học phần môn học")
@@ -40,19 +91,15 @@ button_file = Button(w, text="Chọn file", command=open_file_dialog)
 button_file.pack()
 
 frame_data = Frame(w)
-frame_data.pack(pady=20)
+frame_data.pack()
+
+button_cre = Button(w, text="Tạo báo cáo", command=create_repost)
+button_cre.pack()
+
+label_result = Label(w, text="", justify='left', font=('Helvetica',10, 'bold'))
+label_result.pack()
 
 w.mainloop()
-
-print('Tong so sinh vien di thi :')
-sv = in_data[:,1]
-tongsv = sv.sum()
-print(tongsv)
-print("Ty le % sinh vien dat :") #thêm tỷ lệ phần trăm
-svF = in_data[:,10]
-svDat = np.subtract(sv,svF)
-tyle = np.divide(np.sum(svDat),np.sum(sv))*100
-print(str(tyle) + "%")
 
 # diemA = in_data[:,3]
 # diemBc = in_data[:,4]
@@ -85,11 +132,3 @@ print(str(tyle) + "%")
 # print('Tổng số sinh viên đi thi: {0}'.format(so_sv_thi))
 # print('Tổng số sinh viên thi đạt: {0}'.format(so_sv_dat))
 # print('Tổng số sinh viên thi trượt: {0}'.format(so_sv_truot))
-
-# plt.plot(range(len(diemA)),diemA,'r-',label="Diem A")
-# plt.plot(range(len(diemBc)),diemBc,'g-',label="Diem B +")
-# plt.xlabel('Lơp')
-# plt.ylabel(' So sv dat diem ')
-# plt.legend(loc='upper right')
-# plt.show()
-      
